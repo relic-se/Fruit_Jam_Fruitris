0378a82994fa76aa44047e63757ea32bfe7ccdc2
RetiredWizard <github@retiredwizard.com>
2025-09-17 17:17:30 -0400

    remove print left in by mistake....

--- src/code.py
+++ src/code.py
@@ -168,7 +168,6 @@ except ImportError:
 increment_loading_bar()
 
 # setup audio, buttons, and neopixels
-print()
 peripherals = adafruit_fruitjam.peripherals.Peripherals(
     audio_output=(config.audio_output if config is not None else "headphones"),
     safe_volume_limit=(config.audio_volume_override_danger if config is not None else 12),
